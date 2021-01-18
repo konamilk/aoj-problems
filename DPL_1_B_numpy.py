bd5de3174b905817ce461173650ff0a31cde7e2a
小林拓希 <pinana@PinanaMacBook-Air.local>
2021-01-18 15:58:01 +0900

    solve DPL_1_B

---
+++ DPL_1_B_numpy.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+N, W = map(int, input().split())
+
+v = []
+w = []
+
+W_max = 12000
+
+dp = np.zeros((N+1, W_max), dtype=int)
+
+for i in range(N):
+    vi, wi = map(int, input().split())
+    v.append(vi)
+    w.append(wi)
+
+for i in range(N+1):
+    if i == 0:
+        continue
+
+    for j in range(W_max):
+        if j < w[i-1]:
+            dp[i, j] = dp[i-1, j]
+        else:
+            dp[i, j] = max(dp[i-1, j], dp[i-1, j-w[i-1]] + v[i-1])
+
+print(dp[N, W])
